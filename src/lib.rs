30683d439b2f560da78c6bc18b1f9cf1f8503f6d
Jan Bujak <j@exia.io>
2017-11-29 23:15:36 +0100

    Export `EventListenerHandle`

--- src/lib.rs
+++ src/lib.rs
@@ -126,7 +126,7 @@ pub mod web {
     };
     pub use webapi::cross_origin_setting::CrossOriginSetting;
     pub use webapi::date::Date;
-    pub use webapi::event_target::{IEventTarget, EventTarget};
+    pub use webapi::event_target::{IEventTarget, EventTarget, EventListenerHandle};
     pub use webapi::node::{INode, Node, CloneKind};
     pub use webapi::element::{IElement, Element};
     pub use webapi::html_element::{IHtmlElement, HtmlElement};
