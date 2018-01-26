b04059997c3a9f251537c3032d3c10059910eff6
teovoinea <voinea.teodor@gmail.com>
2018-01-26 14:36:37 -0500

    Add arcTo

--- src/webapi/rendering_context.rs
+++ src/webapi/rendering_context.rs
@@ -40,6 +40,25 @@ impl CanvasRenderingContext2d {
             @{&self.0}.arc(@{x}, @{y}, @{start_angle}, @{end_angle}, @{anticlockwise});
         }
     }
+    /// Adds an arc to the path with the given control points and radius.
+    /// The arc drawn will be a part of a circle, never elliptical. 
+    /// Typical use could be making a rounded corner.
+    /// One way to think about the arc drawn is to imagine two straight segments, from the 
+    /// starting point (latest point in current path) to the first control point, and then 
+    /// from the first control point to the second control point. These two segments form 
+    /// a sharp corner with the first control point being in the corner. Using arcTo, the 
+    /// corner will instead be an arc with the given radius.
+    /// The arc is tangential to both segments, which can sometimes produce surprising results, 
+    /// e.g. if the radius given is larger than the distance between the starting point and the first control point.
+    /// If the radius specified doesn't make the arc meet the starting point (latest point in the current path), 
+    /// the starting point is connected to the arc with a straight line segment.
+    /// 
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arcTo)
+    pub fn arc_to(&self, x1: f64, y1: f64, x2: f64, y2: f64, radius: f64) {
+        js! { @(no_return)
+            @{&self.0}.arcTo(@{x1}, @{y1}, @{x2}, @{y2}, @{radius});
+        }
+    }
     /// Draws a filled rectangle whose starting point is at the coordinates (x, y) with the
     /// specified width and height and whose style is determined by the fillStyle attribute.
     /// 
