0526d33e5766921999887042cf32b632deaf70e1
teovoinea <voinea.teodor@gmail.com>
2018-02-03 23:25:01 -0500

    CanvasPattern intentionally unimplemented because it's still largely experiemental

--- src/webapi/rendering_context.rs
+++ src/webapi/rendering_context.rs
@@ -26,6 +26,7 @@ pub struct CanvasGradient(Reference);
 
 /// The CanvasPattern interface represents an opaque object describing a pattern, based on an image, 
 /// a canvas or a video, created by the CanvasRenderingContext2D.createPattern() method.
+/// Intentionally blank, no non-experimental properties or methods.
 /// 
 /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/CanvasPattern)
 pub struct CanvasPattern(Reference);
