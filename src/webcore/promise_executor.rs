f8b0118c2072d23642c02ff8a8ad2d5c43aaad03
Diggory Blake <diggsey@googlemail.com>
2018-02-09 23:16:51 +0000

    Alternative executor implementation

--- src/webcore/promise_executor.rs
+++ src/webcore/promise_executor.rs
@@ -3,180 +3,85 @@ use futures::executor::{self, Notify, Spawn};
 use futures::Async;
 use std::result::Result as StdResult;
 use std::cell::{Cell, RefCell};
+use std::rc::Rc;
+use webcore::once::Once;
 
-
-#[derive(Clone, Copy)]
-enum TaskState {
-    Idle,
-    Running,
-    Queued,
+// This functionality should really be in libstd, because the implementation
+// looks stupid.
+unsafe fn clone_raw<T>(ptr: *const T) -> Rc<T> {
+    let result = Rc::from_raw(ptr);
+    ::std::mem::forget(result.clone());
+    result
 }
 
+// Typing this out is tedious
+type BoxedFuture = Box< Future< Item = (), Error = () > + 'static >;
+
 struct SpawnedTask {
-    state: Cell< TaskState >,
-    // TODO use Rc<SpawnedTask> instead ?
-    ref_count: Cell< usize >,
-    spawn: RefCell< Spawn< Box< Future< Item = (), Error = () > + 'static > > >,
+    is_queued: Cell< bool >,
+    spawn: RefCell< Option< Spawn< BoxedFuture > >  >,
 }
 
 impl SpawnedTask {
-    fn new< F >( future: F ) -> Self
+    fn new< F >( future: F ) -> Rc<Self>
         where F: Future< Item = (), Error = () > + 'static {
-        Self {
-            state: Cell::new( TaskState::Idle ),
-            ref_count: Cell::new( 1 ),
-            spawn: RefCell::new( executor::spawn(
-                Box::new( future ) as Box< Future< Item = (), Error = () > + 'static >
-            ) ),
-        }
+        Rc::new(Self {
+            is_queued: Cell::new( false ),
+            spawn: RefCell::new( Some( executor::spawn(
+                Box::new( future ) as BoxedFuture
+            ) ) ),
+        })
     }
 
-    // These are the situations that this algorithm needs to worry about:
-    //
-    //   execute -> poll
-    //   execute -> poll + poll
-    //   execute -> poll + notify -> repoll
-    //   execute -> poll + notify + notify -> repoll
-    //   execute -> poll + notify -> repoll -> async notify -> repoll
-    //   execute -> poll -> async notify -> repoll
-    //   execute -> poll -> async notify -> repoll + notify -> repoll
-    //   execute -> poll -> async notify -> repoll -> async notify -> repoll
-    //
-    unsafe fn execute_spawn( spawned_ptr: *const SpawnedTask ) {
-        let spawned = &*spawned_ptr;
+    fn poll(&self) {
+        // Clear `is_queued` flag
+        self.is_queued.set(false);
 
-        loop {
-            // This is necessary because the Future might call `task.notify()` inside of `poll`.
-            // If that happens, we need to re-run `poll` again. So we use `state` to indicate
-            // whether we need to re-run `poll` or not.
-            spawned.state.set( TaskState::Running );
+        let mut spawn = self.spawn.borrow_mut();
 
-            // Here we try to call `poll` on the future. This may not be possible,
-            // as the future may already be executed somewhere higher up in the stack.
-            // We know there is at least one execution scheduled, so it's styled more
-            // like a do-while loop.
-            //
-            // The usage of the lock needs to be contained in a scope that is
-            // separate from the decrement_ref_count, as that can deallocate the
-            // whole spawned task, causing a segfault when the RefCell is trying
-            // to release its borrow. That's why the poll_future_notify call is
-            // contained inside the map.
-            let result = spawned
-                .spawn
-                .try_borrow_mut()
-                .map( |mut s| s.poll_future_notify( &CORE, spawned_ptr as usize ) );
-
-            // TODO test this
-            if let Ok( result ) = result {
-                if let Ok( Async::NotReady ) = result {
-                    // The `poll` method called `task.notify()` so we have to re-run `poll` again.
-                    if let TaskState::Queued = spawned.state.get() {
-                        continue;
-
-                    } else {
-                        // The Future isn't ready yet, but it will asynchronously
-                        // call `task.notify()` when it is ready.
-                        //
-                        // When that happens, the `notify` function will call
-                        // `execute_spawn` again.
-                        spawned.state.set( TaskState::Idle );
-                    }
-
-                } else {
-                    // This ensures that the Future will never be polled again.
-                    spawned.state.set( TaskState::Running );
-
-                    // The whole object might be deallocated at this point, so
-                    // it would be very dangerous to touch anything else.
-                    SpawnedTask::decrement_ref_count( spawned_ptr as usize );
-                }
+        // Take the future so that if we panic it gets dropped
+        if let Some(mut spawn_future) = spawn.take() {
+            if spawn_future.poll_future_notify( &&Core, self as *const _ as usize ) == Ok(Async::NotReady) {
+                // Future was not ready, so put it back
+                *spawn = Some(spawn_future);
             }
-
-            return;
         }
     }
 
-    unsafe fn notify( spawned_ptr: *const SpawnedTask ) {
-        let spawned = &*spawned_ptr;
-
-        // This causes it to re-run `poll` again, even if `task.notify()` was called inside of `poll`.
-        //
-        // IMPORTANT: If the Future calls `notify` multiple times within `poll`, it will only re-run
-        //            `poll` once!
-        let state = spawned.state.replace( TaskState::Queued );
-
-        // This only happens when `task.notify()` is called asynchronously.
-        if let TaskState::Idle = state {
-            SpawnedTask::execute_spawn( spawned_ptr );
-        }
-    }
-
-    unsafe fn increment_ref_count( id: usize ) -> usize {
-        let spawned_ptr = id as *const SpawnedTask;
-        let spawned = &*spawned_ptr;
-        let mut count = spawned.ref_count.get();
-        count += 1;
-        spawned.ref_count.set( count );
-        id
-    }
-
-    unsafe fn decrement_ref_count( id: usize ) {
-        let count = {
-            let spawned_ptr = id as *const SpawnedTask;
-            let spawned = &*spawned_ptr;
-            let mut count = spawned.ref_count.get();
-            count -= 1;
-            spawned.ref_count.set( count );
-            count
-        };
-
-        if count == 0 {
-            let spawned_ptr = id as *mut SpawnedTask;
-
-            // This causes the SpawnedTask to be dropped at the end of the scope.
-            let spawned = Box::from_raw( spawned_ptr );
-
-            // This ensures that the Future will never be polled again.
-            spawned.state.set( TaskState::Running );
+    fn notify( spawned: Rc<SpawnedTask> ) {
+        // If not already queued
+        if !spawned.is_queued.replace(true) {
+            // Poll on next micro-task
+            js! { @(no_return)
+                Promise.resolve().then(function() {
+                    @{ Once(move || spawned.poll()) }();
+                });
+            }
         }
     }
 }
-
-
-static CORE: &Core = &Core;
 
 struct Core;
 
 impl< F > Executor< F > for Core where
     F: Future< Item = (), Error = () > + 'static {
     fn execute( &self, future: F ) -> StdResult< (), ExecuteError< F > > {
-        let spawned_ptr = Box::into_raw( Box::new( SpawnedTask::new( future ) ) );
-
-        unsafe {
-            SpawnedTask::execute_spawn( spawned_ptr );
-        }
-
+        SpawnedTask::notify( SpawnedTask::new( future ) );
         Ok( () )
     }
 }
 
 impl Notify for Core {
     fn notify( &self, spawned_id: usize ) {
-        unsafe {
-            SpawnedTask::notify( spawned_id as *const SpawnedTask );
-        }
+        SpawnedTask::notify(unsafe { clone_raw(spawned_id as *const _) })
     }
 
     fn clone_id( &self, id: usize ) -> usize {
-        unsafe {
-            SpawnedTask::increment_ref_count( id )
-        }
+        unsafe { Rc::into_raw(clone_raw(id as *const SpawnedTask)) as usize }
     }
 
     fn drop_id( &self, id: usize ) {
-        unsafe {
-            SpawnedTask::decrement_ref_count( id );
-        }
+        unsafe { Rc::from_raw(id as *const SpawnedTask) };
     }
 }
 
@@ -184,5 +89,5 @@ impl Notify for Core {
 #[inline]
 pub fn spawn< F >( future: F ) where
     F: Future< Item = (), Error = () > + 'static {
-    CORE.execute( future ).unwrap();
+    Core.execute( future ).unwrap();
 }
