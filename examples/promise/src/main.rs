48924f94d14de638f010f39a2db609c5056258ad
Pauan <pcxunlimited@gmail.com>
2018-02-10 03:11:28 -1000

    Enabling the RefCell example

--- examples/promise/src/main.rs
+++ examples/promise/src/main.rs
@@ -1,21 +1,25 @@
-/*#[cfg(target_arch = "wasm32")]
 #[macro_use]
 extern crate stdweb;
 extern crate futures;
 
-#[cfg(not(target_arch = "wasm32"))]
-extern crate tokio;
-
+use futures::Future;
+use stdweb::unstable::{TryInto};
+use stdweb::web::error::Error;
+use stdweb::{Null, Promise, PromiseFuture};
 use std::rc::Rc;
 use std::cell::RefCell;
-use futures::Future;
 use futures::{Poll, Async};
 use futures::task::{current, Task};
 
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    use stdweb::{PromiseFuture};
 
+fn log( a: &str ) {
+    js! { @(no_return)
+        console.log( @{a} );
+    }
+}
+
+
+fn test_refcell() {
     struct TaskA {
         shared_state: Rc<RefCell<u32>>,
         task_b: Task,
@@ -82,199 +86,99 @@ fn main() {
 }
 
 
-#[cfg(not(target_arch = "wasm32"))]
-fn main() {
-    use tokio::executor::current_thread;
-
-    struct TaskA {
-        shared_state: Rc<RefCell<u32>>,
-        task_b: Task,
-    }
-
-    impl Future for TaskA {
-        type Item = ();
-        type Error = ();
-
-        fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            println!("Poll TaskA");
-
-            let foo = self.shared_state.borrow_mut();
-
-            println!("TaskA 1: {:#?}", foo);
-
-            self.task_b.notify();
-
-            println!("TaskA 2: {:#?}", foo);
-
-            Ok(Async::NotReady)
-        }
-    }
-
-    struct TaskB {
-        shared_state: Rc<RefCell<u32>>,
-        initialized: bool,
-    }
-
-    impl Future for TaskB {
-        type Item = ();
-        type Error = ();
-
-        fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            println!("Poll TaskB");
-
-            if !self.initialized {
-                self.initialized = true;
-
-                let task_b = current();
-
-                let foo = self.shared_state.borrow();
-
-                println!("TaskB 1: {:#?}", foo);
-
-                current_thread::spawn(TaskA {
-                    shared_state: self.shared_state.clone(),
-                    task_b: task_b,
-                });
-            }
-
-            let foo = self.shared_state.borrow();
-
-            println!("TaskB 1: {:#?}", foo);
-
-            Ok(Async::NotReady)
-        }
-    }
-
-    current_thread::run(|_| {
-        current_thread::spawn(TaskB {
-            shared_state: Rc::new(RefCell::new(0)),
-            initialized: false
-        });
-    });
-}*/
-
-
-
-
-#[macro_use]
-extern crate stdweb;
-extern crate futures;
-
-use futures::Future;
-use stdweb::unstable::{TryInto};
-use stdweb::web::error::Error;
-use stdweb::{Null, Promise, PromiseFuture};
-
-
-fn sleep( ms: u32 ) -> PromiseFuture< Null > {
-    js!( return new Promise( function ( success, failure ) {
-        setTimeout( function () {
-            success( null );
-        }, @{ms} );
-    } ); ).try_into().unwrap()
-}
-
-
-fn log( a: &str ) {
-    js! { @(no_return)
-        console.log( @{a} );
-    }
-}
-
-
-struct MyFuture {
-    polls: u32,
-    count: u32,
-    done: bool,
-    receiver: futures::unsync::oneshot::Receiver< () >,
-}
-
-impl MyFuture {
-    fn new( count: u32 ) -> Self {
-        let ( sender, receiver ) = futures::unsync::oneshot::channel();
-
-        let callback = || {
-            log( "setTimeout done" );
-
-            log( &format!("Sending {:#?}", sender.send( () ) ) );
-        };
-
-        log( "setTimeout started" );
-
-        js! { @(no_return)
-            setTimeout( function () {
-                @{stdweb::Once( callback )}();
-            }, 1000 );
-        }
-
-        Self {
-            polls: 0,
-            count: count,
-            done: false,
-            receiver,
-        }
-    }
-}
-
-impl Future for MyFuture {
-    type Item = u32;
-    type Error = ();
-
-    fn poll( &mut self ) -> futures::Poll< Self::Item, Self::Error > {
-        self.polls += 1;
-
-        if !self.done {
-            match self.receiver.poll() {
-                Ok( futures::Async::Ready( () ) ) => self.done = true,
-
-                Ok( futures::Async::NotReady ) => {},
-
-                Err( _ ) => self.done = true,
-            }
-        }
-
-        if self.done {
-            if self.count == 0 {
-                Ok( futures::Async::Ready( self.polls ) )
-
-            } else {
-                self.count -= 1;
-
-                let task = futures::task::current();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-                task.notify();
-
-                Ok( futures::Async::NotReady )
-            }
-
-        } else {
-            Ok( futures::Async::NotReady )
-        }
-    }
-}
-
-
-fn main() {
-    stdweb::initialize();
-
+fn test_panic() {
     let promise: Promise = js!( return Promise.resolve(null); ).try_into().unwrap();
 
     promise.done( |result: Result< Null, Error >| {
         log( &format!( "Promise result: {:#?}", result ) );
         panic!( "Testing panic!" );
     } );
+}
+
+
+fn test_notify() {
+    struct MyFuture {
+        polls: u32,
+        count: u32,
+        done: bool,
+        receiver: futures::unsync::oneshot::Receiver< () >,
+    }
+
+    impl MyFuture {
+        fn new( count: u32 ) -> Self {
+            let ( sender, receiver ) = futures::unsync::oneshot::channel();
+
+            let callback = || {
+                log( "setTimeout done" );
+
+                log( &format!("Sending {:#?}", sender.send( () ) ) );
+            };
+
+            log( "setTimeout started" );
+
+            js! { @(no_return)
+                setTimeout( function () {
+                    @{stdweb::Once( callback )}();
+                }, 1000 );
+            }
+
+            Self {
+                polls: 0,
+                count: count,
+                done: false,
+                receiver,
+            }
+        }
+    }
+
+    impl Future for MyFuture {
+        type Item = u32;
+        type Error = ();
+
+        fn poll( &mut self ) -> futures::Poll< Self::Item, Self::Error > {
+            self.polls += 1;
+
+            if !self.done {
+                match self.receiver.poll() {
+                    Ok( futures::Async::Ready( () ) ) => self.done = true,
+
+                    Ok( futures::Async::NotReady ) => {},
+
+                    Err( _ ) => self.done = true,
+                }
+            }
+
+            if self.done {
+                if self.count == 0 {
+                    Ok( futures::Async::Ready( self.polls ) )
+
+                } else {
+                    self.count -= 1;
+
+                    let task = futures::task::current();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+                    task.notify();
+
+                    Ok( futures::Async::NotReady )
+                }
+
+            } else {
+                Ok( futures::Async::NotReady )
+            }
+        }
+    }
 
     PromiseFuture::spawn(
         MyFuture::new( 5 ).map( |x| {
@@ -282,6 +186,17 @@ fn main() {
             assert_eq!( x, 7 );
         } )
     );
+}
+
+
+fn test_timeout() {
+    fn sleep( ms: u32 ) -> PromiseFuture< Null > {
+        js!( return new Promise( function ( success, failure ) {
+            setTimeout( function () {
+                success( null );
+            }, @{ms} );
+        } ); ).try_into().unwrap()
+    }
 
     PromiseFuture::spawn(
         sleep( 2000 ).inspect( |_| log( "Timeout 1 done!") ).join(
@@ -292,6 +207,16 @@ fn main() {
                 futures::future::err( Error::new( "Testing error!" ) ) )
             .map_err( |e| e.print() )
     );
+}
+
+
+fn main() {
+    stdweb::initialize();
+
+    test_refcell();
+    test_panic();
+    test_notify();
+    test_timeout();
 
     stdweb::event_loop();
 }
