ad81e8fe47fb8b9c58971d8db0102a7b75ff81b1
Pauan <pcxunlimited@gmail.com>
2018-02-11 10:16:11 -1000

    Adding in documentation for the console_* macros

--- src/webapi/console.rs
+++ src/webapi/console.rs
@@ -10,6 +10,17 @@ macro_rules! js_call {
 }
 
 
+/// Clears the console.
+///
+/// If it cannot clear the console then it does nothing.
+///
+/// # Examples
+///
+/// ```rust
+/// console_clear!();
+/// ```
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/clear)
 #[macro_export]
 macro_rules! console_clear {
     () => {
@@ -17,6 +28,56 @@ macro_rules! console_clear {
     };
 }
 
+
+/// Prints zero or more values to the console.
+///
+/// The values can be anything that can be sent to JavaScript, and they do not
+/// need to be the same type.
+///
+/// Usually it will print a space in between each value, and it will always print
+/// a newline at the end.
+///
+/// However, this is *not guaranteed*, the printing behavior **will** vary between
+/// different browsers and Node.js, therefore you should use `console_log` *only*
+/// for debugging purposes.
+///
+/// If the first value is a string, then you can use string substitutions to
+/// control how the remaining values are printed. Please see [this site](https://developer.mozilla.org/en-US/docs/Web/API/console#Using_string_substitutions)
+/// for more information about string substitutions.
+///
+/// # Examples
+///
+/// Print a newline:
+///
+/// ```rust
+/// console_log!("");
+/// ```
+///
+/// Print one value:
+///
+/// ```rust
+/// console_log!("Hello world!");
+/// ```
+///
+/// Print more than one value:
+///
+/// ```rust
+/// console_log!(1, "test", vec![2, 3]);
+/// ```
+///
+/// Print more than one value with Rust's formatting:
+///
+/// ```rust
+/// console_log!(format!("{:#?} {:#?} {:#?}", 1, "test", vec![2, 3]));
+/// ```
+///
+/// Use string substitution to control how the values are printed:
+///
+/// ```rust
+/// console_log!("foo: %s bar: %s", vec![1, 2], vec![3, 4]);
+/// ```
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/log)
 #[macro_export]
 macro_rules! console_log {
     ( $( $args:expr ),* ) => {
@@ -24,6 +85,12 @@ macro_rules! console_log {
     };
 }
 
+
+/// Prints zero or more values to the console.
+///
+/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "error" message.
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/error)
 #[macro_export]
 macro_rules! console_error {
     ( $( $args:expr ),* ) => {
@@ -31,6 +98,15 @@ macro_rules! console_error {
     };
 }
 
+
+/// Prints zero or more values to the console.
+///
+/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "debug" message.
+///
+/// In some JavaScript environments it won't print anything at all, so do not use it
+/// for anything other than debugging.
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/debug)
 #[macro_export]
 macro_rules! console_debug {
     ( $( $args:expr ),* ) => {
@@ -38,6 +114,12 @@ macro_rules! console_debug {
     };
 }
 
+
+/// Prints zero or more values to the console.
+///
+/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "info" message.
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/info)
 #[macro_export]
 macro_rules! console_info {
     ( $( $args:expr ),* ) => {
