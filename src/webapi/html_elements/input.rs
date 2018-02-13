3b85c941db00b8e3c942624afd50c5929085fb08
Jan Bujak <j@exia.io>
2018-02-13 22:05:24 +0100

    Temporarily remove `set_kind` and `files` from `InputElement`
    The original JS API for <input> elements in pretty much horrible.
    We can do better. But not now. Later. So let's remove these for now.

--- src/webapi/html_elements/input.rs
+++ src/webapi/html_elements/input.rs
@@ -1,10 +1,9 @@
-use webcore::value::{Value, Reference};
+use webcore::value::Reference;
 use webcore::try_from::TryInto;
 use webapi::event_target::{IEventTarget, EventTarget};
 use webapi::node::{INode, Node};
 use webapi::element::{IElement, Element};
 use webapi::html_element::{IHtmlElement, HtmlElement};
-use webapi::file_list::FileList;
 
 /// The HTML input element is used to create interactive controls
 /// for web-based forms in order to accept data from the user.
@@ -35,22 +34,5 @@ impl InputElement {
         js! { @(no_return)
             @{self}.value = @{value.into()};
         }
-    }
-
-    /// The type of control to render. See [Form <input> types](https://developer.mozilla.org/en/docs/Web/HTML/Element/input#Form_<input>_types)
-    /// for the individual types, with links to more information about each.
-    #[inline]
-    pub fn set_kind( &self, kind: &str ) {
-        js! { @(no_return)
-            @{self}.type = @{kind};
-        }
-    }
-
-    /// Returns the list of selected files. **Only for inputs of type `file`**.
-    #[inline]
-    pub fn files( &self ) -> Option< FileList > {
-        js! (
-            return @{self}.files;
-        ).try_into().ok()
     }
 }
