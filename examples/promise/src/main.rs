2434825ddf098053d221da2ccdb55859242d3e81
Pauan <pcxunlimited@gmail.com>
2018-02-13 06:42:22 -1000

    Changing to use a single console macro

--- examples/promise/src/main.rs
+++ examples/promise/src/main.rs
@@ -17,7 +17,7 @@ fn test_from_thenable() {
 
     a.unwrap().done( |result: Result< u32, u32 > | {
         assert_eq!( result, Ok( 5 ) );
-        console_log!( format!( "Thenable 1: {:#?}", result ) );
+        console!( log, format!( "Thenable 1: {:#?}", result ) );
     } );
 
 
@@ -25,7 +25,7 @@ fn test_from_thenable() {
 
     a.unwrap().done( |result: Result< u32, u32 > | {
         assert_eq!( result, Ok( 1 ) );
-        console_log!( format!( "Thenable 2: {:#?}", result ) );
+        console!( log, format!( "Thenable 2: {:#?}", result ) );
     } );
 
 
@@ -40,18 +40,18 @@ fn test_error_conversion() {
 
     PromiseFuture::spawn(
         a.map( |_| () ).map_err( |x| {
-            console_log!( "String error:", x );
+            console!( log, "String error:", x );
         } )
     );
 
     let _a: PromiseFuture< Null > = js!( return Promise.resolve( null ); ).try_into().unwrap();
-    console_log!( "Null works" );
+    console!( log, "Null works" );
 
     let _a: PromiseFuture< Null > = js!( return Promise.reject( new Error( "hi!" ) ); ).try_into().unwrap();
-    console_log!( "Error works" );
+    console!( log, "Error works" );
 
     //let _a: PromiseFuture< Null, SyntaxError > = js!( return Promise.reject( new Error( "hi!" ) ); ).try_into().unwrap();
-    //console_log!( "Error conversion fails" );
+    //console!( log, "Error conversion fails" );
 }
 
 
@@ -66,15 +66,15 @@ fn test_refcell() {
         type Error = ();
 
         fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            console_log!("Poll TaskA");
+            console!( log, "Poll TaskA" );
 
             let foo = self.shared_state.borrow_mut();
 
-            console_log!(format!("TaskA 1: {:#?}", foo));
+            console!( log, format!("TaskA 1: {:#?}", foo) );
 
             self.task_b.notify();
 
-            console_log!(format!("TaskA 2: {:#?}", foo));
+            console!( log, format!("TaskA 2: {:#?}", foo) );
 
             Ok(Async::NotReady)
         }
@@ -90,7 +90,7 @@ fn test_refcell() {
         type Error = ();
 
         fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            console_log!("Poll TaskB");
+            console!( log, "Poll TaskB" );
 
             if !self.initialized {
                 self.initialized = true;
@@ -99,7 +99,7 @@ fn test_refcell() {
 
                 let foo = self.shared_state.borrow();
 
-                console_log!(format!("TaskB 1: {:#?}", foo));
+                console!( log, format!("TaskB 1: {:#?}", foo) );
 
                 PromiseFuture::spawn(TaskA {
                     shared_state: self.shared_state.clone(),
@@ -109,7 +109,7 @@ fn test_refcell() {
 
             let foo = self.shared_state.borrow();
 
-            console_log!(format!("TaskB 1: {:#?}", foo));
+            console!( log, format!("TaskB 1: {:#?}", foo) );
 
             Ok(Async::NotReady)
         }
@@ -126,7 +126,7 @@ fn test_panic() {
     let promise: Promise = js!( return Promise.resolve(null); ).try_into().unwrap();
 
     promise.done( |result: Result< Null, Error >| {
-        console_log!( format!( "Promise result: {:#?}", result ) );
+        console!( log, format!( "Promise result: {:#?}", result ) );
         panic!( "Testing panic!" );
     } );
 }
@@ -145,12 +145,12 @@ fn test_notify() {
             let ( sender, receiver ) = futures::unsync::oneshot::channel();
 
             let callback = || {
-                console_log!( "setTimeout done" );
+                console!( log, "setTimeout done" );
 
-                console_log!( format!("Sending {:#?}", sender.send( () ) ) );
+                console!( log, format!("Sending {:#?}", sender.send( () ) ) );
             };
 
-            console_log!( "setTimeout started" );
+            console!( log, "setTimeout started" );
 
             js! { @(no_return)
                 setTimeout( function () {
@@ -218,7 +218,7 @@ fn test_notify() {
 
     PromiseFuture::spawn(
         MyFuture::new( 5 ).map( |x| {
-            console_log!( format!( "MyFuture count: {}", x ) );
+            console!( log, format!( "MyFuture count: {}", x ) );
             assert_eq!( x, 7 );
         } )
     );
@@ -235,13 +235,13 @@ fn test_timeout() {
     }
 
     PromiseFuture::spawn(
-        sleep( 2000 ).inspect( |_| console_log!( "Timeout 1 done!" ) ).join(
-        sleep( 2000 ).inspect( |_| console_log!( "Timeout 2 done!" ) ) )
+        sleep( 2000 ).inspect( |_| console!( log, "Timeout 1 done!" ) ).join(
+        sleep( 2000 ).inspect( |_| console!( log, "Timeout 2 done!" ) ) )
             .and_then( |_|
-                sleep( 1000 ).inspect( |_| console_log!( "Timeout 3 done!" ) ) )
+                sleep( 1000 ).inspect( |_| console!( log, "Timeout 3 done!" ) ) )
             .and_then( |_|
                 futures::future::err( Error::new( "Testing error!" ) ) )
-            .map_err( |e| console_error!( e ) )
+            .map_err( |e| console!( error, e ) )
     );
 }
 
@@ -258,4 +258,3 @@ fn main() {
 
     stdweb::event_loop();
 }
-
--- src/webapi/console.rs
+++ src/webapi/console.rs
@@ -1,141 +1,67 @@
-#[doc(hidden)]
-#[macro_export]
-macro_rules! js_call {
-    ( $f:expr, $( $args:expr ),* ) => {{
-        js! { @(no_return)
-            $f( $( @{$args} ),* );
-        }
-        ()
-    }};
-}
-
-
-/// Clears the console.
+/// Calls methods on the JavaScript `console` object.
 ///
-/// If it cannot clear the console then it does nothing.
+/// This should **only** be used for debugging purposes, its behavior is
+/// **not** standardized: it **will** vary with different browsers
+/// and Node.js.
+///
+/// The first argument is the name of the `console` method.
+///
+/// The remaining arguments can be anything which can be sent to JavaScript,
+/// and they do not need to be the same type.
+///
+/// If you want to print things to the console in a standardized way, use
+/// [`println!`](https://doc.rust-lang.org/std/macro.println.html) instead.
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/console)
 ///
 /// # Examples
 ///
+/// ## clear
+///
+/// Clear the console:
+///
 /// ```rust
-/// console_clear!();
+/// console!(clear);
 /// ```
 ///
 /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/clear)
-#[macro_export]
-macro_rules! console_clear {
-    () => {
-        js_call!( console.clear, );
-    };
-}
-
-
-/// Prints zero or more values to the console.
 ///
-/// The values can be anything that can be sent to JavaScript, and they do not
-/// need to be the same type.
-///
-/// Usually it will print a space in between each value, and it will always print
-/// a newline at the end.
-///
-/// However, this is *not guaranteed*, the printing behavior **will** vary between
-/// different browsers and Node.js, therefore you should use `console_log` *only*
-/// for debugging purposes.
-///
-/// If the first value is a string, then you can use string substitutions to
-/// control how the remaining values are printed. Please see [this site](https://developer.mozilla.org/en-US/docs/Web/API/console#Using_string_substitutions)
-/// for more information about string substitutions.
-///
-/// # Examples
+/// ## log
 ///
 /// Print a newline:
 ///
 /// ```rust
-/// console_log!("");
+/// console!(log, "");
 /// ```
 ///
 /// Print one value:
 ///
 /// ```rust
-/// console_log!("Hello world!");
+/// console!(log, "Hello world!");
 /// ```
 ///
 /// Print more than one value:
 ///
 /// ```rust
-/// console_log!(1, "test", vec![2, 3]);
+/// console!(log, 1, "test", vec![2, 3]);
 /// ```
 ///
-/// Print more than one value with Rust's formatting:
-///
-/// ```rust
-/// console_log!(format!("{:#?} {:#?} {:#?}", 1, "test", vec![2, 3]));
-/// ```
-///
-/// Use string substitution to control how the values are printed:
-///
-/// ```rust
-/// console_log!("foo: %s bar: %s", vec![1, 2], vec![3, 4]);
-/// ```
+/// All of the `log` examples also work with `error`, `debug`, `info`, and `warn`.
 ///
 /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/log)
 #[macro_export]
-macro_rules! console_log {
-    ( $( $args:expr ),* ) => {
-        js_call!( console.log, $( $args ),* );
-    };
-}
+macro_rules! console {
+    ( $name:ident ) => {{
+        js! { @(no_return)
+            console.$name();
+        }
+        ()
+    }};
 
-
-/// Prints zero or more values to the console.
-///
-/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "error" message.
-///
-/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/error)
-#[macro_export]
-macro_rules! console_error {
-    ( $( $args:expr ),* ) => {
-        js_call!( console.error, $( $args ),* );
-    };
-}
-
-
-/// Prints zero or more values to the console.
-///
-/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "debug" message.
-///
-/// In some JavaScript environments it won't print anything at all, so do not use it
-/// for anything other than debugging.
-///
-/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/debug)
-#[macro_export]
-macro_rules! console_debug {
-    ( $( $args:expr ),* ) => {
-        js_call!( console.debug, $( $args ),* );
-    };
-}
-
-
-/// Prints zero or more values to the console.
-///
-/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "info" message.
-///
-/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/info)
-#[macro_export]
-macro_rules! console_info {
-    ( $( $args:expr ),* ) => {
-        js_call!( console.info, $( $args ),* );
-    };
-}
-
-
-/// Prints zero or more values to the console.
-///
-/// This is exactly the same as [`console_log`](macro.console_log.html), except that it prints a special "warn" message.
-///
-/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Console/warn)
-#[macro_export]
-macro_rules! console_warn {
-    ( $( $args:expr ),* ) => {
-        js_call!( console.warn, $( $args ),* );
-    };
+    ( $name:ident, $( $args:expr ),* ) => {{
+        js! { @(no_return)
+            console.$name( $( @{$args} ),* );
+        }
+        ()
+    }};
 }
