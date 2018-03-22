b9ed6a273bcb11b0c3e3a0dbbf1ce0281f98a55b
Teodor Voinea <voinea.teodor@gmail.com>
2018-03-22 15:37:35 -0400

    Add `Element::get_bounding_client_rect` (#144)

--- src/webapi/html_element.rs
+++ src/webapi/html_element.rs
@@ -5,6 +5,88 @@ use webapi::node::{INode, Node};
 use webapi::element::{IElement, Element};
 use webapi::string_map::StringMap;
 
+/// Represents a rectangle.
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRect)
+// https://drafts.fxtf.org/geometry-1/#domrect
+#[derive(Clone, Debug, ReferenceType)]
+#[reference(instance_of = "DOMRect")]
+pub struct Rect (Reference);
+
+impl Rect {
+
+    /// Represents the x coordinate of the DOMRect's origin
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/x)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrect-x
+    pub fn get_x( &self ) -> f64 {
+        js! (
+            return @{&self.0}.x;
+        ).try_into().unwrap()
+    }
+
+    /// Represents the y coordinate of the DOMRect's origin.
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/y)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrect-y
+    pub fn get_y( &self ) -> f64 {
+        js! (
+            return @{&self.0}.y;
+        ).try_into().unwrap()
+    }
+
+    /// Represents the width of the DOMRect.
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/width)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrect-width
+    pub fn get_width( &self ) -> f64 {
+        js! (
+            return @{&self.0}.width;
+        ).try_into().unwrap()
+    }
+
+    /// Represents the height of the DOMRect.
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/height)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrect-height
+    pub fn get_height( &self ) -> f64 {
+        js! (
+            return @{&self.0}.height;
+        ).try_into().unwrap()
+    }
+
+    /// Returns the top coordinate value of the DOMRect. (Has the same value as y, or y + height if height is negative.)
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/top)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrectreadonly-top
+    pub fn get_top( &self ) -> f64 {
+        js! (
+            return @{&self.0}.top;
+        ).try_into().unwrap()
+    }
+
+    /// Returns the right coordinate value of the DOMRect. (Has the same value as x + width, or x if width is negative.)
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/right)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrectreadonly-right
+    pub fn get_right( &self ) -> f64 {
+        js! (
+            return @{&self.0}.right;
+        ).try_into().unwrap()
+    }
+
+    /// Returns the bottom coordinate value of the DOMRect. (Has the same value as y + height, or y if height is negative.)
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/bottom)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrectreadonly-bottom
+    pub fn get_bottom( &self ) -> f64 {
+        js! (
+            return @{&self.0}.bottom;
+        ).try_into().unwrap()
+    }
+
+    /// Returns the left coordinate value of the DOMRect. (Has the same value as x, or x + width if width is negative.)
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/DOMRectReadOnly/left)
+    // https://drafts.fxtf.org/geometry-1/#dom-domrectreadonly-left
+    pub fn get_left( &self ) -> f64 {
+        js! (
+            return @{&self.0}.left;
+        ).try_into().unwrap()
+    }
+}
+
 /// The `IHtmlElement` interface represents any HTML element.
 ///
 /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement)
@@ -41,6 +123,15 @@ pub trait IHtmlElement: IElement {
                 return @{self.as_ref()}.dataset;
             ).into_reference_unchecked().unwrap()
         }
+    }
+
+    /// Returns the size of an element and its position relative to the viewport.
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect)
+    // https://drafts.csswg.org/cssom-view/#ref-for-dom-element-getboundingclientrect
+    fn get_bounding_client_rect( &self ) -> Rect {
+        js! (
+            return @{self.as_ref()}.getBoundingClientRect();
+        ).try_into().unwrap()
     }
 
     /// Returns the layout width of an element. Typically, an element's offsetWidth is a
