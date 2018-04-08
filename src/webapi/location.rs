e448750e147579d590a42453a722705f1fbe2286
Garrett Berg <googberg@gmail.com>
2018-04-07 20:03:07 -0600

    address review comments

--- src/webapi/location.rs
+++ src/webapi/location.rs
@@ -18,21 +18,13 @@ use webapi::dom_exception::SecurityError;
 #[reference(instance_of = "Location")]
 pub struct Location( Reference );
 
-macro_rules! jsattr {
-    [$s:ident, $attr:ident] => {{
-        js_try!(
-            return @{$s}.$attr;
-        ).unwrap()
-    }}
-}
-
 impl Location {
     /// The entire URL.
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/href)
-    // https://html.spec.whatwg.org/#dom-location-href
+    // https://html.spec.whatwg.org/#the-location-interface:dom-location-href
     pub fn href( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, href)
+        js_try!( return @{self}.href; ).unwrap()
     }
 
     /// Returns a `String` containing the Unicode serialization of the origin of the represented
@@ -48,7 +40,7 @@ impl Location {
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/origin)
     // https://html.spec.whatwg.org/#dom-location-origin
     pub fn origin( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, origin)
+        js_try!( return @{self}.origin; ).unwrap()
     }
 
     /// Returns a `String` representing the protocol scheme of the URL, including the final ':'.
@@ -56,9 +48,9 @@ impl Location {
     /// Example: `http:`
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/protocol)
-    // https://html.spec.whatwg.org/#dom-location-protocol
+    // https://html.spec.whatwg.org/#the-location-interface:dom-location-protocol
     pub fn protocol( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, protocol)
+        js_try!( return @{self}.protocol; ).unwrap()
     }
 
     /// Returns a `String` containing the host (i.e. hostname) and then, if the port of the
@@ -69,7 +61,7 @@ impl Location {
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/host)
     // https://html.spec.whatwg.org/#dom-location-host
     pub fn host( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, host)
+        js_try!( return @{self}.host; ).unwrap()
     }
 
     /// Returns a `String` which is the domain of the URL
@@ -77,25 +69,25 @@ impl Location {
     /// Example: `mozilla.com`
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/hostname)
-    // https://html.spec.whatwg.org/#dom-location-hostname
+    // https://html.spec.whatwg.org/#the-location-interface:dom-location-hostname
     pub fn hostname( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, hostname)
+        js_try!( return @{self}.hostname; ).unwrap()
     }
 
     /// Returns a `String` containing the port number or `""` if there is no port.
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/port)
-    // https://html.spec.whatwg.org/#dom-location-port
+    // https://html.spec.whatwg.org/#the-location-interface:dom-location-port
     pub fn port( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, port)
+        js_try!( return @{self}.port; ).unwrap()
     }
 
     /// Returns a `String` containing an initial '/' followed by the path of the URL.
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/pathname)
-    // https://html.spec.whatwg.org/#dom-location-pathname
+    // https://html.spec.whatwg.org/#the-location-interface:dom-location-pathname
     pub fn pathname( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, pathname)
+        js_try!( return @{self}.pathname; ).unwrap()
     }
 
     /// Returns a `String` which is a search string, also called a query string, that is a `String`
@@ -104,9 +96,9 @@ impl Location {
     /// These can then be further parsed via another library.
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/search)
-    // https://html.spec.whatwg.org/#dom-location-search
+    // https://html.spec.whatwg.org/#the-location-interface:dom-location-search
     pub fn search( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, search)
+        js_try!( return @{self}.search; ).unwrap()
     }
 
     /// Returns a `String` containing a '#' followed by the fragment
@@ -115,6 +107,6 @@ impl Location {
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Location/hash)
     // https://html.spec.whatwg.org/#the-location-interface:dom-location-hash
     pub fn hash( &self ) -> Result< String, SecurityError > {
-        jsattr!(self, hash)
+        js_try!( return @{self}.hash; ).unwrap()
     }
 }
