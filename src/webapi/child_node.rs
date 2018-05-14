4be5487bb934b593309a1ee5521be553e265cf9f
kzvi <kzvi@users.noreply.github.com>
2018-05-14 13:44:01 -0700

    fix link in IChildNode::remove documentation

--- src/webapi/child_node.rs
+++ src/webapi/child_node.rs
@@ -12,7 +12,7 @@ pub trait IChildNode: ReferenceType {
     /// The `ChildNode.remove()` method removes the object from the tree it belongs to.
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove)
-    // https://dom.spec.whatwg.org/#dom-childnode-remove
+    // https://dom.spec.whatwg.org/#ref-for-dom-childnode-remove
     fn remove( &self ) {
         js! { @(no_return)
             @{self.as_ref()}.remove();
