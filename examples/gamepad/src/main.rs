c1ef686e2d716359cc9c5aa5f98552c1b68f954b
Cory Sherman <coryshrmn@gmail.com>
2018-05-25 21:32:42 -0700

    cleanup gamepad example

--- examples/gamepad/src/main.rs
+++ examples/gamepad/src/main.rs
@@ -1,16 +1,12 @@
 extern crate stdweb;
 
-use std::rc::Rc;
-use std::cell::RefCell;
-
 use stdweb::traits::*;
-use stdweb::unstable::TryInto;
 use stdweb::web::{
     document,
+    Element,
     Gamepad,
     GamepadMappingType,
     get_gamepads,
-    HtmlElement,
     IEventTarget,
     IGamepad,
     IGamepadButton,
@@ -22,85 +18,112 @@ use stdweb::web::event::{
     IGamepadEvent,
 };
 
+/// Create an element and set its content.
+fn elem_content(elem_type: &str, content: &str) -> Element {
+    let elem = document().create_element(elem_type).unwrap();
+    elem.set_text_content(content);
+    elem
+}
+
+/// Write a new line to the "log" div.
 fn log(msg: &str) {
-    let log_div: HtmlElement = document().query_selector( ".log" ).unwrap().unwrap().try_into().unwrap();
+    let log_div = document().query_selector("#log").unwrap().unwrap();
 
-    let p = document().create_element("p").unwrap();
-    p.set_text_content(msg);
-    log_div.append_child(&p);
+    log_div.append_child(&elem_content("p", msg));
 }
 
-struct State {
-    gamepads: Vec<Gamepad>,
-    div: HtmlElement,
+fn get_pad_title(pad: &Gamepad) -> Element {
+    let div = document().create_element("div").unwrap();
+
+    div.append_child(&elem_content("h2",
+        &format!("Pad {}: {}", pad.index(), pad.id())
+    ));
+
+    div.append_child(&elem_content("h3",
+        if pad.connected() { "Connected" } else { "Disconnected" }
+    ));
+
+    div.append_child(&elem_content("h3",
+        &format!("Mapping: {}", match pad.mapping() { GamepadMappingType::Standard => "standard", _ => "non-standard" })
+    ));
+
+    div.append_child(&elem_content("h3",
+        &format!("Last updated: {:.0}", pad.timestamp())
+    ));
+
+    div
 }
 
-impl State {
+fn get_pad_axes(pad: &Gamepad) -> Element {
+    let div = document().create_element("div").unwrap();
 
-    fn new(div: HtmlElement) -> Self {
-        Self {
-            gamepads: vec![],
-            div,
+    for (i, a) in pad.axes().into_iter().enumerate() {
+        let elem = elem_content("p",
+            &format!("Axis {}: {}", i, a)
+        );
+
+        if a != 0.0 {
+            elem.set_attribute("class", "gp-pressed").unwrap();
+        }
+
+        div.append_child(&elem);
+    }
+
+    div
+}
+
+fn get_pad_buttons(pad: &Gamepad) -> Element {
+    let div = document().create_element("div").unwrap();
+
+    for (i, b) in pad.buttons().into_iter().enumerate() {
+        let elem = elem_content("p",
+            &format!("Button {}: Pressed = {}; Value = {}", i, b.pressed(), b.value())
+        );
+
+        if b.value() != 0.0 {
+            elem.set_attribute("class", "gp-pressed").unwrap();
+        }
+
+        div.append_child(&elem);
+    }
+
+    div
+}
+
+fn get_pad_state(pad: &Option<Gamepad>) -> Element {
+    let elem = document().create_element("div").unwrap();
+
+    match pad {
+        Some(pad) => {
+            elem.append_child(&get_pad_title(&pad));
+            elem.append_child(&get_pad_axes(&pad));
+            elem.append_child(&get_pad_buttons(&pad));
+        },
+        None => {
+            elem.append_child(&elem_content("h2", "No pad"));
         }
     }
 
-    fn add(&mut self, pad: Gamepad) {
-        self.gamepads.push(pad);
+    elem
+}
+
+/// Update gamepad state view
+fn animate() {
+    let list = document().create_element("ul").unwrap();
+
+    for pad in get_gamepads() {
+        let item = document().create_element("li").unwrap();
+        item.append_child(&get_pad_state(&pad));
+        list.append_child(&item);
     }
 
-    fn remove(&mut self, pad: Gamepad) {
-        self.gamepads.retain(|p| p.index() != pad.index());
-    }
+    let state = document().query_selector("#state").unwrap().unwrap();
 
-    fn animate(&self, rc: Rc<RefCell<Self>>) {
+    state.set_text_content("");
+    state.append_child(&list);
 
-        // Chrome only updates Gamepad state when getGamepads is called
-        get_gamepads();
-
-        let list = document().create_element("ul").unwrap();
-
-        for (i, pad) in self.gamepads.iter().enumerate() {
-            let item = document().create_element("li").unwrap();
-
-            let title = format!("pad[{}] \"{}\" {}; {} mapping; last update = {:.0}",
-                i,
-                pad.id(),
-                if pad.connected() { "connected" } else { "disconnected" },
-                match pad.mapping() { GamepadMappingType::Standard => "standard", _ => "nonstandard" },
-                pad.timestamp()
-            );
-
-            let title_p = document().create_element("p").unwrap();
-            title_p.set_text_content(&title);
-
-            item.append_child(&title_p);
-
-            for (i, a) in pad.axes().into_iter().enumerate() {
-                let axis = document().create_element("p").unwrap();
-                axis.set_text_content(&format!("axis[{}] = {}", i, a));
-                item.append_child(&axis);
-            }
-
-            for (i, b) in pad.buttons().into_iter().enumerate() {
-                let button = document().create_element("p").unwrap();
-                button.set_text_content(&format!("button[{}] pressed = {}; value = {}",
-                    i,
-                    b.pressed(),
-                    b.value()
-                ));
-                item.append_child(&button);
-            }
-
-            list.append_child(&item);
-        }
-
-        self.div.set_text_content("");
-        self.div.append_child(&list);
-
-        window().request_animation_frame(move |_| {
-            rc.borrow().animate(rc.clone());
-        });
-    }
+    // queue another animate() on the next frame
+    window().request_animation_frame(|_| animate());
 }
 
 fn main() {
@@ -108,30 +131,18 @@ fn main() {
 
     log("Waiting for gamepad connection...");
 
-    let state_div: HtmlElement = document().query_selector( ".state" ).unwrap().unwrap().try_into().unwrap();
-
-    let state_rc = Rc::new(RefCell::new(State::new(state_div)));
-
-    state_rc.borrow().animate(state_rc.clone());
-
-    let state_rc1 = state_rc.clone();
+    animate();
 
     window().add_event_listener( move |e: GamepadConnectedEvent| {
         let pad = e.gamepad();
-
         let message = format!("gamepad \"{}\" connected", pad.id());
         log(&message);
-
-        state_rc1.borrow_mut().add(pad);
     });
 
     window().add_event_listener( move |e: GamepadDisconnectedEvent| {
         let pad = e.gamepad();
-
         let message = format!("gamepad \"{}\" disconnected", pad.id());
         log(&message);
-
-        state_rc.borrow_mut().remove(pad);
     });
 
     stdweb::event_loop();
