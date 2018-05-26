1dcd7b762d5260e45d601b7fb8da8d06b97ddb69
Cory Sherman <coryshrmn@gmail.com>
2018-05-25 19:03:56 -0700

    finish Gamepad members + GamepadButton
    The example listens for gamepad connections, and constantly displays the full state of all gamepads.
    
    Works on Firefox 60.0.1.
    
    Not updating properly on Chrome 65.0.3325.181.
    The gamepad state is correct when first connected, but never updates.

--- examples/gamepad/src/main.rs
+++ examples/gamepad/src/main.rs
@@ -1,32 +1,133 @@
-#[macro_use]
 extern crate stdweb;
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use stdweb::traits::*;
+use stdweb::unstable::TryInto;
 use stdweb::web::{
+    document,
+    Gamepad,
+    GamepadMappingType,
+    HtmlElement,
     IEventTarget,
     IGamepad,
+    IGamepadButton,
     window,
 };
-
 use stdweb::web::event::{
     GamepadConnectedEvent,
+    GamepadDisconnectedEvent,
     IGamepadEvent,
 };
+
+fn log(msg: &str) {
+    let log_div: HtmlElement = document().query_selector( ".log" ).unwrap().unwrap().try_into().unwrap();
+
+    let p = document().create_element("p").unwrap();
+    p.set_text_content(msg);
+    log_div.append_child(&p);
+}
+
+struct State {
+    gamepads: Vec<Gamepad>,
+    div: HtmlElement,
+}
+
+impl State {
+
+    fn new(div: HtmlElement) -> Self {
+        Self {
+            gamepads: vec![],
+            div,
+        }
+    }
+
+    fn add(&mut self, pad: Gamepad) {
+        self.gamepads.push(pad);
+    }
+
+    fn remove(&mut self, pad: Gamepad) {
+        self.gamepads.retain(|p| p.index() != pad.index());
+    }
+
+    fn animate(&self, rc: Rc<RefCell<Self>>) {
+
+        let list = document().create_element("ul").unwrap();
+
+        for (i, pad) in self.gamepads.iter().enumerate() {
+            let item = document().create_element("li").unwrap();
+
+            let title = format!("pad[{}] \"{}\" {}; {} mapping; last update = {:.0}",
+                i,
+                pad.id(),
+                if pad.connected() { "connected" } else { "disconnected" },
+                match pad.mapping() { GamepadMappingType::Standard => "standard", _ => "nonstandard" },
+                pad.timestamp()
+            );
+
+            let title_p = document().create_element("p").unwrap();
+            title_p.set_text_content(&title);
+
+            item.append_child(&title_p);
+
+            for (i, a) in pad.axes().into_iter().enumerate() {
+                let axis = document().create_element("p").unwrap();
+                axis.set_text_content(&format!("axis[{}] = {}", i, a));
+                item.append_child(&axis);
+            }
+
+            for (i, b) in pad.buttons().into_iter().enumerate() {
+                let button = document().create_element("p").unwrap();
+                button.set_text_content(&format!("button[{}] pressed = {}; value = {}",
+                    i,
+                    b.pressed(),
+                    b.value()
+                ));
+                item.append_child(&button);
+            }
+
+            list.append_child(&item);
+        }
+
+        self.div.set_text_content("");
+        self.div.append_child(&list);
+
+        window().request_animation_frame(move |_| {
+            rc.borrow().animate(rc.clone());
+        });
+    }
+}
 
 fn main() {
     stdweb::initialize();
 
-    let message = "hello rust!";
+    log("Waiting for gamepad connection...");
 
-    js! {
-        alert( @{message} );
-    }
+    let state_div: HtmlElement = document().query_selector( ".state" ).unwrap().unwrap().try_into().unwrap();
+
+    let state_rc = Rc::new(RefCell::new(State::new(state_div)));
+
+    state_rc.borrow().animate(state_rc.clone());
+
+    let state_rc1 = state_rc.clone();
 
     window().add_event_listener( move |e: GamepadConnectedEvent| {
-        let message = format!("gamepad \"{}\" connected in rust!", e.gamepad().id());
+        let pad = e.gamepad();
 
-        js! {
-            alert( @{message} );
-        }
+        let message = format!("gamepad \"{}\" connected", pad.id());
+        log(&message);
+
+        state_rc1.borrow_mut().add(pad);
+    });
+
+    window().add_event_listener( move |e: GamepadDisconnectedEvent| {
+        let pad = e.gamepad();
+
+        let message = format!("gamepad \"{}\" disconnected", pad.id());
+        log(&message);
+
+        state_rc.borrow_mut().remove(pad);
     });
 
     stdweb::event_loop();
--- src/webapi/gamepad.rs
+++ src/webapi/gamepad.rs
@@ -1,8 +1,15 @@
 //use std::fmt::Debug;
 
 use webcore::reference_type::ReferenceType;
-use webcore::try_from::TryInto;
-use webcore::value::Reference;
+use webcore::try_from::{
+    TryFrom,
+    TryInto,
+};
+use webcore::value::{
+    ConversionError,
+    Reference,
+    Value,
+};
 
 /// The set of known gamepad layout mappings.
 ///
@@ -16,6 +23,72 @@ pub enum GamepadMappingType {
     Standard,
 }
 
+impl TryFrom<Value> for GamepadMappingType {
+    type Error = ConversionError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v {
+            Value::String(s) => match s.as_ref() {
+                "" => Ok(GamepadMappingType::NoMapping),
+                "standard" => Ok(GamepadMappingType::Standard),
+                s => Err(ConversionError::Custom(format!("invalid gamepad mapping type \"{}\"", s))),
+            },
+            _ => Err(ConversionError::type_mismatch(&v)),
+        }
+    }
+}
+
+/// The state of an individual button on a gamepad device.
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/GamepadButton)
+// https://w3c.github.io/gamepad/#gamepadbutton-interface
+pub trait IGamepadButton: ReferenceType {
+
+    /// Is the button currently pressed?
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/GamepadButton/pressed)
+    // https://w3c.github.io/gamepad/#dom-gamepadbutton-pressed
+    #[inline]
+    fn pressed(&self) -> bool {
+        js!(
+            return @{self.as_ref()}.pressed;
+        ).try_into().unwrap()
+    }
+
+    /// Is the button currently touched?
+    ///
+    /// MDN does not document this, it may be unsupported by browsers.
+    // https://w3c.github.io/gamepad/#dom-gamepadbutton-touched
+    #[inline]
+    fn touched(&self) -> bool {
+        js!(
+            return @{self.as_ref()}.touched;
+        ).try_into().unwrap()
+    }
+
+    /// The amount which the button has been pressed, between 0.0 (not pressed), and 1.0 (fully pressed).
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/GamepadButton/value)
+    // https://w3c.github.io/gamepad/#dom-gamepadbutton-touched
+    #[inline]
+    fn value(&self) -> f64 {
+        js!(
+            return @{self.as_ref()}.value;
+        ).try_into().unwrap()
+    }
+}
+
+/// A reference to a JavaScript object which implements the [IGamepadButton](trait.IGamepadButton.html)
+/// interface.
+///
+/// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/GamepadButton)
+// https://w3c.github.io/gamepad/#gamepadbutton-interface
+#[derive(Clone, Debug, Eq, PartialEq, ReferenceType)]
+#[reference(instance_of = "GamepadButton")]
+pub struct GamepadButton( Reference );
+
+impl IGamepadButton for GamepadButton {}
+
 /// An individual gamepad/controller.
 ///
 /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad)
@@ -25,7 +98,7 @@ pub trait IGamepad: ReferenceType {
     /// A string containing some information about this gamepad.
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/id)
-    // https://w3c.github.io/gamepad/#gamepad-interface
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-id
     #[inline]
     fn id(&self) -> String {
         js!(
@@ -33,8 +106,72 @@ pub trait IGamepad: ReferenceType {
         ).try_into().unwrap()
     }
 
-}
+    /// An auto-incrementing integer to uniquely identify a connected Gamepad.
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/index)
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-index
+    #[inline]
+    fn index(&self) -> i32 {
+        js!(
+            return @{self.as_ref()}.index;
+        ).try_into().unwrap()
+    }
 
+    /// Is this gamepad connected to the system, powered on, and usable?
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/connected)
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-connected
+    #[inline]
+    fn connected(&self) -> bool {
+        js!(
+            return @{self.as_ref()}.connected;
+        ).try_into().unwrap()
+    }
+
+    /// Monotonically increasing time this gamepad was updated.
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/timestamp)
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-timestamp
+    #[inline]
+    fn timestamp(&self) -> f64 {
+        js!(
+            return @{self.as_ref()}.timestamp;
+        ).try_into().unwrap()
+    }
+
+    /// The mapping in use for this device.
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/mapping)
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-mapping
+    #[inline]
+    fn mapping(&self) -> GamepadMappingType {
+        js!(
+            return @{self.as_ref()}.mapping;
+        ).try_into().unwrap()
+    }
+
+    /// Array of values for all axes of the gamepad.
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/axes)
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-axes
+    #[inline]
+    fn axes(&self) -> Vec<f64> {
+        js!(
+            return @{self.as_ref()}.axes;
+        ).try_into().unwrap()
+    }
+
+    /// Array of button states for all buttons of the gamepad.
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Gamepad/buttons)
+    // https://www.w3.org/TR/gamepad/#dom-gamepad-buttons
+    #[inline]
+    fn buttons(&self) -> Vec<GamepadButton> {
+        js!(
+            return @{self.as_ref()}.buttons;
+        ).try_into().unwrap()
+    }
+}
 
 /// A reference to a JavaScript object which implements the [IGamepad](trait.IGamepad.html)
 /// interface.
