66fd193e5a7a40eac3242f0f9b995a120d21d1bd
Ubuntu <joeknockando@googlemail.com>
2018-05-28 23:59:09 +0000

    Get rid of all the warings in the example code.

--- examples/indexeddb/src/main.rs
+++ examples/indexeddb/src/main.rs
@@ -8,24 +8,20 @@ extern crate serde;
 
 use std::collections::HashMap;
 use std::cell::RefCell;
-use std::str::FromStr;
 
 use stdweb::Value;
 
 use stdweb::traits::*;
 use stdweb::web::{
-    HtmlElement,
     Element,
     window,
     document,
     IEventTarget,
-    EventTarget,
-    Node
 };
 
 use stdweb::web::html_element::InputElement;
 
-use stdweb::web::event::{ConcreteEvent, IEvent};
+use stdweb::web::event::IEvent;
 
 use stdweb::web::event::{
     IDBSuccessEvent,
@@ -41,8 +37,6 @@ use stdweb::web::indexeddb::{
     IDBDatabase,
     IDBRequest,
     DBRequest,
-    IDBCursor,
-    IDBObjectStore,
     IDBObjectStoreIndexSharedMethods,
     IDBCursorWithValue,
     IDBCursorSharedMethods
@@ -67,7 +61,7 @@ fn display_data_inner(db: &IDBDatabase) {
     // Here we empty the contents of the list element each time the display is updated
     // If you ddn't do this, you'd get duplicates listed each time a new note is added
     while list.first_child().is_some() {
-        list.remove_child(&list.first_child().unwrap());
+       list.remove_child(&list.first_child().unwrap()).unwrap();
     }
     // Open our object store and then get a cursor - which iterates through all the
     // different data items in the store
@@ -83,13 +77,13 @@ fn display_data_inner(db: &IDBDatabase) {
             if let Ok(cursor) = maybe_cursor {
                 // Create a list item, h3, and p to put each data item inside when displaying it
                 // structure the HTML fragment, and append it inside the list
-                let listItem = document().create_element("li").unwrap();
+                let list_item = document().create_element("li").unwrap();
                 let h3 = document().create_element("h3").unwrap();
                 let para = document().create_element("p").unwrap();
                 
-                listItem.append_child(&h3);
-                listItem.append_child(&para);
-                list.append_child(&listItem);
+                list_item.append_child(&h3);
+                list_item.append_child(&para);
+                list.append_child(&list_item);
 
                 let note: Note = cursor.value().try_into().unwrap();
                 
@@ -97,28 +91,28 @@ fn display_data_inner(db: &IDBDatabase) {
                 h3.set_text_content(&note.title);
                 para.set_text_content(&note.body);
                 
-                // Store the ID of the data item inside an attribute on the listItem, so we know
+                // Store the ID of the data item inside an attribute on the list_item, so we know
                 // which item it corresponds to. This will be useful later when we want to delete items
                 let id: u32 = cursor.key().try_into().unwrap();
-                listItem.set_attribute("data-note-id", &format!("{}", id));
-                // Create a button and place it inside each listItem
-                let deleteBtn = document().create_element("button").unwrap();
-                listItem.append_child(&deleteBtn);
-                deleteBtn.set_text_content("Delete");
+                list_item.set_attribute("data-note-id", &format!("{}", id)).unwrap();
+                // Create a button and place it inside each list_item
+                let delete_btn = document().create_element("button").unwrap();
+                list_item.append_child(&delete_btn);
+                delete_btn.set_text_content("Delete");
                 
                 // Set an event handler so that when the button is clicked, the deleteItem()
                 // function is run
-                deleteBtn.add_event_listener( delete_item );
+                delete_btn.add_event_listener( delete_item );
 
                 // Iterate to the next item in the cursor
                 cursor.advance(1); // Todo this was continue
                 
             } else {
                 // Again, if list item is empty, display a 'No notes stored' message
-                if(list.first_child().is_none()) {
-                    let listItem = document().create_element("li").unwrap();
-                    listItem.set_text_content("No notes stored.");
-                    list.append_child(&listItem);
+                if list.first_child().is_none() {
+                    let list_item = document().create_element("li").unwrap();
+                    list_item.set_text_content("No notes stored.");
+                    list.append_child(&list_item);
                 }
                 // if there are no more cursor items to iterate through, say so
                 console!(log, "Notes all displayed");
@@ -139,29 +133,29 @@ fn delete_item( e: ClickEvent ) {
     // values are type-sensitive.
     let button: Element = e.target().unwrap().try_into().unwrap();
     let note: Element = button.parent_node().unwrap().try_into().unwrap();
-    let noteId = note.get_attribute("data-note-id").unwrap().parse::<u32>().unwrap();
+    let note_id = note.get_attribute("data-note-id").unwrap().parse::<u32>().unwrap();
     
     // open a database transaction and delete the task, finding it using the id we retrieved above
     DB.with(|db_cell| {
         if let Some(ref db) = *db_cell.borrow_mut()  {
             let transaction = db.transaction("notes", "readwrite");
-            let objectStore = transaction.object_store("notes");
-            let request = objectStore.delete(noteId.try_into().unwrap());
+            let object_store = transaction.object_store("notes");
+            object_store.delete(note_id.try_into().unwrap());
             
             // report that the data item has been deleted
-            transaction.add_event_listener( move |e: IDBCompleteEvent| {
+            transaction.add_event_listener( move |_e: IDBCompleteEvent| {
                 // delete the parent of the button
                 // which is the list item, so it is no longer displayed
                 //let node: Node = e.target().unwrap().try_into().unwrap();
-                note.parent_node().unwrap().remove_child(&note);
-                console!(log, "Note ", noteId,  "deleted.");
+                note.parent_node().unwrap().remove_child(&note).unwrap();
+                console!(log, "Note ", note_id,  "deleted.");
 
                 // Again, if list item is empty, display a 'No notes stored' message
                 let list = document().query_selector("ul").unwrap().unwrap();
-                if(!list.first_child().is_some()) {
-                    let listItem = document().create_element("li").unwrap();
-                    listItem.set_text_content("No notes stored.");
-                    list.append_child(&listItem);
+                if ! list.first_child().is_some() {
+                    let list_item = document().create_element("li").unwrap();
+                    list_item.set_text_content("No notes stored.");
+                    list.append_child(&list_item);
                 }
             });
         }});
@@ -170,16 +164,12 @@ fn delete_item( e: ClickEvent ) {
 fn main() {
     stdweb::initialize();
     
-    let submit_btn = document().query_selector("form button");
-
- 
-    
     // Open our database; it is created if it doesn't already exist
     // (see onupgradeneeded below)
     let request = window().indexed_db().open("notes", 1);
 
     // onerror handler signifies that the database didn't open successfully
-    request.add_event_listener( |event: IDBErrorEvent| {
+    request.add_event_listener( | _e: IDBErrorEvent| {
         js!(
             console.log("Database failed to open");
         );
@@ -206,14 +196,14 @@ fn main() {
     	let db_request: IDBOpenDBRequest = event.target().unwrap().try_into().unwrap();
         let db_: IDBDatabase = db_request.result().try_into().unwrap();
 
-        // Create an objectStore to store our notes in (basically like a single table)
+        // Create an object_store to store our notes in (basically like a single table)
         // including a auto-incrementing key
         let mut store_options = HashMap::new();
         //store_options.insert("keyPath", "id");
         store_options.insert("autoIncrement", "true");
         let object_store = db_.create_object_store("notes", Value::from(store_options));
         
-        // Define what data items the objectStore will contain
+        // Define what data items the object_store will contain
         let mut title_options = HashMap::new();
         title_options.insert("unique", false);
         object_store.create_index("title", "title", Value::from(title_options));
@@ -236,7 +226,7 @@ fn main() {
         // grab the values entered into the form fields and store them in an object ready for being inserted into the DB
         let title_input: InputElement = document().query_selector("#title").unwrap().unwrap().try_into().unwrap();
         let body_input: InputElement = document().query_selector("#body").unwrap().unwrap().try_into().unwrap();
-        let newItem = Note{ title: title_input.raw_value(), body: body_input.raw_value() };
+        let new_item = Note{ title: title_input.raw_value(), body: body_input.raw_value() };
 
         DB.with(|db_cell| {
             if let Some(ref db) = *db_cell.borrow_mut() {
@@ -244,42 +234,28 @@ fn main() {
                 let transaction = db.transaction("notes", "readwrite");
         
                 // call an object store that's already been added to the database
-                let objectStore = transaction.object_store("notes");
+                let object_store = transaction.object_store("notes");
         
-                // Make a request to add our newItem object to the object store
-                let request = objectStore.add(newItem.try_into().unwrap(), None);
+                // Make a request to add our new_item object to the object store
+                let request = object_store.add(new_item.try_into().unwrap(), None);
                 
-                request.add_event_listener( move |e: IDBSuccessEvent| {
+                request.add_event_listener( move |_e: IDBSuccessEvent| {
                     // Clear the form, ready for adding the next entry
                     title_input.set_raw_value("");
                     body_input.set_raw_value("");
                 });
         
                 // Report on the success of the transaction completing, when everything is done
-                transaction.add_event_listener( |e: IDBCompleteEvent| {
+                transaction.add_event_listener( |_e: IDBCompleteEvent| {
                     console!(log, "Transaction completed: database modification finished.");
                     
                     // update the display of data to show the newly added item, by running displayData() again.
                     display_data();
                 });
         
-                transaction.add_event_listener( |e: IDBErrorEvent| {
+                transaction.add_event_listener( |_e: IDBErrorEvent| {
                     console!(log, "Transaction not opened due to error");
                 });
-
-
-
-            }});
-
-
-
-
-
-
-
-
-
-        
+            }});        
     });
-    
 }
