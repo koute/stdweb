a73804f1d0e9434788b4977c50dae24cdb540b0e
Pauan <pcxunlimited@gmail.com>
2018-09-17 02:54:41 -1000

    Improving the promise example (adding in demonstration of concurrency)

--- examples/promise/src/main.rs
+++ examples/promise/src/main.rs
@@ -1,7 +1,10 @@
-#![feature(async_await, await_macro, futures_api)]
+#![feature(async_await, await_macro, futures_api, pin)]
 
 #[macro_use]
 extern crate stdweb;
+
+#[macro_use]
+extern crate futures;
 
 use stdweb::{PromiseFuture, spawn_local, unwrap_future};
 use stdweb::web::wait;
@@ -9,23 +12,51 @@ use stdweb::web::error::Error;
 use stdweb::unstable::TryInto;
 
 
+// Converts a JavaScript Promise into a Rust Future
 fn javascript_promise() -> PromiseFuture< u32 > {
     js!(
         return new Promise( function ( success, error ) {
             setTimeout( function () {
                 success( 50 );
-            }, 1000 );
+            }, 2000 );
         } );
     ).try_into().unwrap()
 }
 
 
+async fn print( message: &str ) {
+    // Waits for 2000 milliseconds
+    await!( wait( 2000 ) );
+    console!( log, message );
+}
+
+
 async fn future_main() -> Result< (), Error > {
-    await!( wait( 1000 ) );
-    console!( log, "Hello" );
-    await!( wait( 1000 ) );
-    console!( log, "There" );
-    console!( log, await!( javascript_promise() )? );
+    // Runs Futures synchronously
+    await!( print( "Hello" ) );
+    await!( print( "There" ) );
+
+    {
+        let a = print( "Test 1" );
+        let b = print( "Test 2" );
+
+        // Runs multiple Futures in parallel
+        let (a, b) = join!( a, b );
+
+        console!( log, "Done", a, b );
+    }
+
+    {
+        let a = javascript_promise();
+        let b = javascript_promise();
+
+        // Runs multiple Futures (which can error) in parallel
+        let (a, b) = try_join!( a, b )?;
+
+        console!(log, a);
+        console!(log, b);
+    }
+
     Ok( () )
 }
 
