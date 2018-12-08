31257891c6bad0e385fadfadb641baf88d610cc0
Jan Bujak <j@exia.io>
2018-12-08 23:14:00 +0100

    Do not use custom sections after all
    From what I can see the way Rust currently decides whenever to emit
    a custom section is broken, which makes it impossible to force it
    to do so. This in certain cases will result in JS shims which
    generate an import but which are missing from the custom section.
    
    So we'll temporarily go back to the way we previously used to emit
    JS snippets, except this time we're using procedural macros.

--- stdweb-internal-macros/src/js_shim.rs
+++ stdweb-internal-macros/src/js_shim.rs
@@ -4,13 +4,6 @@ use sha1::Sha1;
 
 use utils::dummy_idents;
 
-#[derive(Clone, Serialize, Deserialize, Debug)]
-struct Snippet {
-    name: String,
-    code: String,
-    arg_count: usize
-}
-
 fn hash( string: &str ) -> String {
     let mut hasher = Sha1::new();
     hasher.update( string.as_bytes() );
@@ -18,33 +11,30 @@ fn hash( string: &str ) -> String {
 }
 
 pub fn js_shim_extern_code( code: &str, arg_count: usize ) -> (syn::Ident, TokenStream) {
-    let snippet = Snippet {
-        name: format!( "__cargo_web_snippet_{}", hash( code ) ),
-        code: code.to_owned(),
-        arg_count
-    };
-
-    let blob: Vec< u8 > = serde_json::to_string( &snippet ).unwrap().into_bytes();
-    let blob_length = blob.len();
-
+    let name = format!( "__cargo_web_snippet_{}", hash( code ) );
     let code_bytes = syn::LitByteStr::new( format!( "{}\0", code ).as_str().as_bytes(), Span::call_site() );
 
-    let shim_name = syn::Ident::new( &snippet.name, Span::call_site() );
+    let shim_name = syn::Ident::new( &name, Span::call_site() );
     let shim_args: Vec< _ > = dummy_idents( arg_count ).map( |name| quote! { #name: *const u8 } ).collect();
     let shim_args = &shim_args;
-    let shim_args_passthrough = dummy_idents( arg_count ).map( |name| quote! { #name } );
+    let shim_args_passthrough: Vec< _ > = dummy_idents( arg_count ).map( |name| quote! { #name } ).collect();
+    let shim_args_passthrough = &shim_args_passthrough;
+
+    let asm_fn_name = syn::Ident::new( &format!( "__js_{}", arg_count ), Span::call_site() );
+    let asm_fn_name = &asm_fn_name;
+
     let output = quote! {
-        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
-        #[link_section = "cargo-web-js"]
-        static SNIPPET: [u8; #blob_length] = [#(#blob),*];
-
-        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
-        extern "C" {
-            pub fn #shim_name( #(#shim_args),* ) -> i32;
-        }
-
-        #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
         const SNIPPET: &'static [u8] = #code_bytes;
+
+        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+        fn #shim_name( #(#shim_args),* ) -> i32 {
+            extern "C" {
+                pub fn #asm_fn_name( #(#shim_args,)* code: *const u8 ) -> i32;
+            }
+            unsafe {
+                #asm_fn_name( #(#shim_args_passthrough,)* SNIPPET as *const _ as *const u8 )
+            }
+        }
 
         #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
         fn #shim_name( #(#shim_args),* ) -> i32 {
