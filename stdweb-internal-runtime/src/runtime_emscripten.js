04ead3c62ca087928d54c9d285827d42d36d7512
pravdomil <pravdomil.toman@gmail.com>
2019-03-14 15:51:55 +0100

    fix Runtime.dynCall is not a function

--- stdweb-internal-runtime/src/runtime_emscripten.js
+++ stdweb-internal-runtime/src/runtime_emscripten.js
@@ -3,7 +3,7 @@ Module.STDWEB_PRIVATE.alloc = function alloc( size ) {
 };
 
 Module.STDWEB_PRIVATE.dyncall = function( signature, ptr, args ) {
-    return Runtime.dynCall( signature, ptr, args );
+    return dynCall( signature, ptr, args );
 };
 
 Module.STDWEB_PRIVATE.utf8_len = function utf8_len( str ) {
