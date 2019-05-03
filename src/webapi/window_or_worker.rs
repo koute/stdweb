149808400c15409c1b8b006de15f8e35c483e3c0
Ryan Goldstein <ryanisaacg@gmail.com>
2019-05-02 21:33:16 -0400

    Add the clear_timeout method to IWindowOrWorker

--- src/webapi/window_or_worker.rs
+++ src/webapi/window_or_worker.rs
@@ -21,12 +21,22 @@ pub trait IWindowOrWorker: ReferenceType {
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
     // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-settimeout
-    fn set_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) {
+    fn set_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) -> i32 {
         let callback = Box::into_raw( Box::new( callback ) );
         __js_raw_asm!( "\
             Module.STDWEB_PRIVATE.acquire_js_reference( $0 ).setTimeout( function() {\
                 Module.STDWEB_PRIVATE.dyncall( 'vi', $1, [$2] );\
             }, $3 );\
-        ", self.as_ref().as_raw(), funcall_adapter::< F > as extern fn( *mut F ), callback, timeout );
+        ", self.as_ref().as_raw(), funcall_adapter::< F > as extern fn( *mut F ), callback, timeout )
+    }
+
+    /// Clears a timer previously established by set_timeout
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearTimeout)
+    // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-clear-timeout
+    fn clear_timeout( &self, id: i32 ) {
+        js! {
+            clearTimeout(@{id})
+        }
     }
 }
