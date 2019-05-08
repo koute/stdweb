2968f8d0c3a769489daf8593f1893c05f240cd77
Ryan Goldstein <ryanisaacg@gmail.com>
2019-05-07 21:54:42 -0400

    Address feedback

--- src/webapi/window_or_worker.rs
+++ src/webapi/window_or_worker.rs
@@ -21,22 +21,36 @@ pub trait IWindowOrWorker: ReferenceType {
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
     // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-settimeout
-    fn set_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) -> i32 {
-        let callback = Box::into_raw( Box::new( callback ) );
-        __js_raw_asm!( "\
-            Module.STDWEB_PRIVATE.acquire_js_reference( $0 ).setTimeout( function() {\
-                Module.STDWEB_PRIVATE.dyncall( 'vi', $1, [$2] );\
-            }, $3 );\
-        ", self.as_ref().as_raw(), funcall_adapter::< F > as extern fn( *mut F ), callback, timeout )
+    fn set_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) {
+        set_clearable_tmeout(callback, timeout);
     }
 
-    /// Clears a timer previously established by set_timeout
+    /// Sets a timer which executes a function once after the timer expires and can be cleared
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
+    // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-settimeout
+    fn set_clearable_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) -> TimeoutHandle {
+        let callback = Box::into_raw( Box::new( callback ) );
+        let id = __js_raw_asm!( "\
+            return Module.STDWEB_PRIVATE.acquire_js_reference( $0 ).setTimeout( function() {\
+                Module.STDWEB_PRIVATE.dyncall( 'vi', $1, [$2] );\
+            }, $3 );\
+        ", self.as_ref().as_raw(), funcall_adapter::< F > as extern fn( *mut F ), callback, timeout );
+
+        TimeoutHandle(id)
+    }
+}
+
+pub struct TimeoutHandle(i32);
+
+impl TimeoutHandle {
+    /// Clears a timer previously established by set_clearable_timeout
     ///
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearTimeout)
     // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-clear-timeout
-    fn clear_timeout( &self, id: i32 ) {
+     pub fn clear( & self ) {
         js! {
-            clearTimeout(@{id})
+            clearTimeout(@{id});
         }
     }
 }
