d1a1e7725c94ad81bcc0d7d1fb2e44830cb28118
Ryan Goldstein <ryanisaacg@gmail.com>
2019-05-29 21:09:29 -0700

    Remove indirection from set_timeout

--- src/webapi/window_or_worker.rs
+++ src/webapi/window_or_worker.rs
@@ -22,7 +22,12 @@ pub trait IWindowOrWorker: ReferenceType {
     /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
     // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-settimeout
     fn set_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) {
-        set_clearable_tmeout(callback, timeout);
+        let callback = Box::into_raw( Box::new( callback ) );
+        __js_raw_asm!( "\
+            Module.STDWEB_PRIVATE.acquire_js_reference( $0 ).setTimeout( function() {\
+                Module.STDWEB_PRIVATE.dyncall( 'vi', $1, [$2] );\
+            }, $3 );\
+        ", self.as_ref().as_raw(), funcall_adapter::< F > as extern fn( *mut F ), callback, timeout );
     }
 
     /// Sets a timer which executes a function once after the timer expires and can be cleared
