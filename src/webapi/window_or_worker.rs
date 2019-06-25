8ae1650fd147f9ac7e50c6a2553e883e5d6098e3
Ryan G <ryanisaacg@users.noreply.github.com>
2019-06-25 14:10:14 -0700

    Add `IWindowOrWorker::set_clearable_timeout` (#337)

--- src/webapi/window_or_worker.rs
+++ src/webapi/window_or_worker.rs
@@ -1,3 +1,5 @@
+use webcore::once::Once;
+use webcore::value::Value;
 use webcore::reference_type::ReferenceType;
 
 extern fn funcall_adapter< F: FnOnce() >( callback: *mut F ) {
@@ -28,5 +30,37 @@ pub trait IWindowOrWorker: ReferenceType {
                 Module.STDWEB_PRIVATE.dyncall( 'vi', $1, [$2] );\
             }, $3 );\
         ", self.as_ref().as_raw(), funcall_adapter::< F > as extern fn( *mut F ), callback, timeout );
+    }
+
+    /// Sets a timer which executes a function once after the timer expires and can be cleared
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
+    // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-settimeout
+    fn set_clearable_timeout< F: FnOnce() + 'static >( &self, callback: F, timeout: u32 ) -> TimeoutHandle {
+        TimeoutHandle(js! (
+            const callback = @{Once(callback)};
+            return {
+                id: setTimeout(callback, @{timeout}),
+                callback
+            };
+        ))
+    }
+}
+
+#[derive(Debug)]
+/// A reference to a timeout object created by set_clearable_timeout
+pub struct TimeoutHandle(Value);
+
+impl TimeoutHandle {
+    /// Clears a timer previously established by set_clearable_timeout
+    ///
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearTimeout)
+    // https://html.spec.whatwg.org/#windoworworkerglobalscope-mixin:dom-clear-timeout
+     pub fn clear( self ) {
+        js! { @(no_return)
+            var val = @{&self.0};
+            clearTimeout(val.id);
+            val.callback.drop();
+        }
     }
 }
