af02fb4a5ecdb8a1763fa71a6a4bb9430b1876a2
Hung-I Wang <Gowee@users.noreply.github.com>
2019-07-16 04:20:42 +0800

    Add `IBlob::slice` and `IBlob::slice_with_content_type` (#349)

--- src/webapi/blob.rs
+++ src/webapi/blob.rs
@@ -1,6 +1,33 @@
+use std::ops::{RangeBounds, Bound};
+
 use webcore::value::Reference;
 use webcore::try_from::TryInto;
 use webcore::reference_type::ReferenceType;
+use webcore::number::Number;
+use webcore::optional_arg::OptionalArg;
+
+// https://w3c.github.io/FileAPI/#ref-for-dfn-slice
+fn slice_blob< T, U >( blob: &T, range: U, content_type: Option< &str > ) -> Blob
+    where T: IBlob, U: RangeBounds< u64 >
+{
+    let start: Number = match range.start_bound() {
+        Bound::Included(&n) => n,
+        Bound::Excluded(&n) => n + 1,
+        Bound::Unbounded => 0
+    }.try_into().unwrap();
+
+    let end: OptionalArg< Number > = match range.end_bound() {
+        Bound::Included(&n) => Some(n + 1),
+        Bound::Excluded(&n) => Some(n),
+        Bound::Unbounded => None
+    }.try_into().unwrap();
+
+    let content_type: OptionalArg< &str > = content_type.into();
+    let reference = blob.as_ref();
+    js! (
+        return @{reference}.slice(@{start}, @{end}, @{content_type});
+    ).try_into().unwrap()
+}
 
 /// A blob object represents a file-like object of immutable, raw data.
 /// Blobs represent data that isn't necessarily in a JavaScript-native format.
@@ -30,6 +57,25 @@ pub trait IBlob: ReferenceType {
         } else {
             Some( mime )
         }
+    }
+
+    /// Create a new `Blob` object containing the data in the specified range of bytes of the
+    /// source `Blob`.
+    /// 
+    /// See also [slice_with_content_type](IBlob::slice_with_content_type).
+    /// 
+    /// [(JavaScript docs)](https://developer.mozilla.org/en-US/docs/Web/API/Blob/slice)
+    fn slice< T >( &self, range: T ) -> Blob
+        where T: RangeBounds<u64>
+    {
+        slice_blob(self, range, None)
+    }
+
+    /// [slice](IBlob::slice) `Blob` with the provided `content_type`.
+    fn slice_with_content_type< T >( &self, range: T, content_type: &str ) -> Blob
+        where T: RangeBounds<u64>
+    {
+        slice_blob(self, range, Some(content_type))
     }
 }
 
